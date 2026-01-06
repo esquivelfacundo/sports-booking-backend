f8c49ce934db5a2a9b7e233ebabdf61ea14fefa6
esquivelfacundo <facundoesquivel01@gmail.com>
2026-01-06 08:08:38 -0300

    fix: Agregar import de jwt en authController
    PROBLEMA:
    - superAdminLogin usaba jwt.sign() pero faltaba el import
    - Causaba error: ReferenceError: jwt is not defined
    
    SOLUCIÓN:
    - Agregar: const jwt = require('jsonwebtoken');
    
    El login de superadmin ahora funcionará correctamente.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,7 @@ const { generateTokenPair, verifyRefreshToken } = require('../utils/jwt');
 const { Op } = require('sequelize');
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { OAuth2Client } = require('google-auth-library');
 const { sendVerificationCode } = require('../services/email');
 
